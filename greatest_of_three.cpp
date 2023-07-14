257aa5fc5d543a24298004acbd88cad562574c63
Priyanshu Gandhi <111220317+priyanshu-gandhi@users.noreply.github.com>
2023-07-14 15:51:06 +0530

    Greatest of three

---
+++ greatest_of_three.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b,c;
+	cout<<"Enter the value of a,b and c\n";
+	cin>>a>>b>>c;
+	
+	if(a>b)
+	  {
+	  	 if(a>c)
+	  	  cout<<"a is greatest\n";
+	  	  else
+	  	  cout<<"c is greatest\n";
+	  }
+	else{
+		if(b>c)
+		   cout<<"b is greatest\n";
+		else
+		   cout<<"c is greatest\n";
+	} 
+
+	
+	return 0;
+}
